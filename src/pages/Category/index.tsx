3bfb4f9e54982467904fcc10accf3e0c15dd3f47
faridasad <asadlifaridd@gmail.com>
2023-10-07 01:38:01 +0400

    Add Category page

---
+++ src/pages/Category/index.tsx
@@ -0,0 +1,13 @@
+import Search from "../../components/Search";
+
+interface CategoryProps {}
+
+const Category: React.FC<CategoryProps> = () => {
+  return (
+    <div className="container">
+        <Search />
+    </div>
+  );
+};
+
+export default Category;
