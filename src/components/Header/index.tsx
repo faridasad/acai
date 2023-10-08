ad4ef7f9ca65e19f939cb203beb47980bc6d2f01
faridasad <asadlifaridd@gmail.com>
2023-10-08 13:19:01 +0400

    Add functionality to back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,20 @@ import LogoImage from "../../assets/images/acai-logo.svg";
 import BackIcon from "../../assets/images/icon-back.svg";
 import "./header.scss";
 
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   return (
-    <header className={`${pathname === "/" && "home"}`}>
+    <header className={`${pathname === "/" ? "home" : ""}`}>
       {pathname === "/" ? (
         <img src={LogoImage} alt="Logo" />
       ) : (
         <div className="container">
           <div className="controls">
-            <button className="back-button">
+            <button className="back-button" onClick={() => navigate(-1)}>
               <img src={BackIcon} alt="back" />
             </button>
             <button className="ask-waiter-button">
