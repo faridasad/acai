7542a45ff24d4f6836e5d595233c8718998ad0a3
faridasad <asadlifaridd@gmail.com>
2023-10-08 13:19:51 +0400

    Update Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,12 +5,13 @@ import HomeData from "../../data/home.json";
 import HomeSlider from "../../components/HomeSlider";
 import Search from "../../components/Search";
 
-import "./home.scss"
+import "./home.scss";
 import { CSSProperties } from "react";
+import { Link } from "react-router-dom";
 
-// Types 
+// Types
 export interface CustomCSS extends CSSProperties {
-  '--_grid-items-count': number;
+  "--_grid-items-count": number;
 }
 
 const Home = () => {
@@ -20,13 +21,23 @@ const Home = () => {
       <div className="container">
         <Search />
         <section className="categories">
-          <ul role="list" className="categories__inner" style={{'--_grid-items-count': HomeData.categories.items.length} as CustomCSS}>
+          <ul
+            role="list"
+            className="categories__inner"
+            style={
+              {
+                "--_grid-items-count": HomeData.categories.items.length,
+              } as CustomCSS
+            }
+          >
             {HomeData.categories.items.map((c, idx) => {
               return (
-                <li className="category-item" key={idx}>
-                  <img src={c.img_url} alt={c.name} />
-                  <span>{c.name}</span>
-                </li>
+                <Link to={`/${c.href}`} key={idx}>
+                  <li className="category-item">
+                    <img src={c.img_url} alt={c.name} />
+                    <span>{c.name}</span>
+                  </li>
+                </Link>
               );
             })}
           </ul>
