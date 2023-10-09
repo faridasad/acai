0379710694d8da0d2d05b561776b22e9139482dd
faridasad <asadlifaridd@gmail.com>
2023-10-09 18:11:58 +0400

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.scss";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import Error from "./pages/Error";
 
 function App() {
   const Layout = () => {
@@ -27,6 +25,8 @@ function App() {
       children: [
         { path: "/", element: <Home /> },
         { path: "/:category", element: <Category /> },
+        { path: "/cart", element: <Cart /> },
+        { path: "*", element: <Error /> },
       ],
     },
   ]);
