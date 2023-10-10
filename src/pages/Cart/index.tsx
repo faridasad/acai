5efe5ccaedde650650a5b26b2f618e4fd0ca0790
faridasad <asadlifaridd@gmail.com>
2023-10-10 18:27:02 +0400

    Add Suggested Items Section in Cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -6,9 +6,15 @@ import { useNavigate } from "react-router-dom";
 import { Drawer } from "vaul";
 import CartListItem from "../../components/CartListItem";
 
+// Dummy Data
+import CategoriesData from "../../data/categories.json";
+
 // Assets and Styles
 import MessageIcon from "../../assets/images/icon-message.svg";
 import "./cart.scss";
+import CategoryListItem from "../../components/CategoryListItem";
+import { useState } from "react";
+import { SelectedProduct } from "../Category";
 
 // Types
 interface CartProps {}
@@ -20,8 +26,27 @@ const Cart: React.FC<CartProps> = () => {
     state.updateNotes,
     state.resetCart,
   ]);
+  const categories = CategoriesData.categories;
+
+  const allProducts = [...categories.salads.items, ...categories.soups.items];
+
+  const cartIngredients = cart.map((p) => p.ingredients).flat();
+
+  const suggestedProducts = allProducts.filter((product) => {
+    // Check if the product's ingredients contain any of the cart ingredients.
+    return (
+      cartIngredients.some((ingredient) => {
+        if (ingredient === undefined) return;
+
+        return product.ingredients.includes(ingredient)
+      }) &&
+      // Check if the product is not already in the cart.
+      !cart.some((cartItem) => cartItem.id === product.id)
+    );
+  });
 
   const navigate = useNavigate();
+  const [selectedProducts, setSelectedProducts] = useState<SelectedProduct[]>([]);
 
   return (
     <section className="cart">
@@ -59,8 +84,8 @@ const Cart: React.FC<CartProps> = () => {
             <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
               <div className="p-3 bg-white rounded-t-[10px] flex-1">
                 <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-accent-100 mb-8" />
-                <div className="max-w-md mx-auto flex flex-col">
-                  <div className="modal-notes">
+                <div className="max-w-xl mx-auto flex flex-col">
+                  <section className="modal-notes">
                     <h2>Notes</h2>
                     <div className="modal-notes__inner">
                       <ul role="list">
@@ -86,7 +111,7 @@ const Cart: React.FC<CartProps> = () => {
                         })}
                       </ul>
                     </div>
-                  </div>
+                  </section>
                 </div>
               </div>
             </Drawer.Content>
@@ -114,6 +139,39 @@ const Cart: React.FC<CartProps> = () => {
       <div className="container">
         <div className="suggested-products">
           <h2>Sizin üçün tövsiyyələrimiz</h2>
+          <ul role="list" className="suggested-products-list">
+            {suggestedProducts.map((product) => {
+              return (
+                <Drawer.Root key={product.id}>
+                  <Drawer.Trigger asChild>
+                    <CategoryListItem
+                      item={product}
+                      isInModal={false}
+                      setSelectedProducts={setSelectedProducts}
+                      selectedProducts={selectedProducts}
+                    />
+                  </Drawer.Trigger>
+                  {/* Draggable modal */}
+                  <Drawer.Portal>
+                    <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+                    <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
+                      <div className="p-3 bg-white rounded-t-[10px] flex-1">
+                        <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-accent-100 mb-8" />
+                        <div className="max-w-md mx-auto flex flex-col">
+                          <CategoryListItem
+                            item={product}
+                            isInModal={true}
+                            setSelectedProducts={setSelectedProducts}
+                            selectedProducts={selectedProducts}
+                          />
+                        </div>
+                      </div>
+                    </Drawer.Content>
+                  </Drawer.Portal>
+                </Drawer.Root>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </section>
